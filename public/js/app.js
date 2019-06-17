7ed3c17af9b1cf2fa885116f8c4d326b4c5b4d18
lhuddlesto <lance299@gmail.com>
2019-06-16 20:27:15 -0400

    Removed unneccessary console logs

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('File is loaded.')
-
 const weatherForm = document.querySelector("form")
 const search = document.querySelector("input")
 const messageOne = document.querySelector('#message-1')
@@ -12,7 +10,6 @@ weatherForm.addEventListener('submit', (e) => {
     messageTwo.textContent = ''
     fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
-            console.log(data)
             if (data.error) {
                 messageOne.textContent = data.error
                 messageTwo.textContent = ''
